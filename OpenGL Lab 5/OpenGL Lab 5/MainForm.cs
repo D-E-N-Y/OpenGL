924e58ffa40644244d21917180cb65d8ddeccb7a
D-E-N-Y <mysliaev.daniil@gmail.com>
2024-10-30 17:07:44 +0200

    upd: create OpenGL Lab 5

---
+++ OpenGL Lab 5/OpenGL Lab 5/MainForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using static OpenGL_Lab_5.OpenGL;
+
+
+namespace OpenGL_Lab_5
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
